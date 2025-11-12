be7e000fbeebdf33a44f1550f409375181493f10
Zhaofei <40413273+ZhaoF-i@users.noreply.github.com>
2025-11-12 19:04:46 +0800

    Update infer.py

--- infer.py
+++ infer.py
@@ -5,8 +5,8 @@ import soundfile as sf
 net = NET()
 net.load_state_dict(torch.load(model.ckpt))
 
-mic, _ = sf.read('')
-farend, _ = sf.read('')
+mic, _ = sf.read('./wav_samples/30DT_mic.wav')
+farend, _ = sf.read('./wav_samples/30DT_far.wav')
 
 mic = torch.Tensor(mic).unsqueeze(dim=0)
 farend = torch.Tensor(farend).unsqueeze(dim=0)
@@ -15,3 +15,4 @@ with torch.no_grad():
     est_time = net(torch.stack([mic, farend], dim=1))
 
 print(est_time.shape)
+
